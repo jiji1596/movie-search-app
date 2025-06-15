f140b77aee3fc6df0ae8f146206afbc2fff493a0
Shunji Sugaya <sugayasc33@gmail.com>
2025-06-15 13:35:19 +0900

    basic searching function done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+function MovieCard({movie}) {
+  return (
+  <div className="movie-card">
+    <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/150"} alt={movie.Title} />
+    <div className="movie-info">
+      <h3>{movie.Title}</h3>
+      <p>{movie.Year}</p>
+      <p className="movie-type">{movie.Type}</p>
+    </div>
+  </div>
+  )
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchInput, setSearchInput] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  function search(event) {
+    setSearchInput(event.currentTarget.value);
+  }
+
+  function searchClick() {
+    const url = `http://www.omdbapi.com/?apikey=b74f1343&s=${searchInput}`
+
+    setSearchQuery(searchInput);
+    fetch(url).then(response => response.json())
+    .then((data) => {
+      setMovies(data.Search);
+    })
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" value={searchInput} onChange={search}/>
+      <button onClick={searchClick}> Search </button>
+      { movies.length > 0 &&
+        <div className="cards">
+          { movies.map((movie, i) => {
+            return (
+              <div key={i}>
+                <MovieCard movie={movie} />
+              </div>
+            )
+          })}
+        </div>
+      }
     </>
   )
 }
